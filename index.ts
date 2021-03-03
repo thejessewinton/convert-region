e8d68b69132a4f87e518752d0f8e8440f428cadd
Jesse Winton <jrandallwinton@gmail.com>
2021-03-03 08:41:15 -0800

    updates

---
+++ index.ts
@@ -0,0 +1,27 @@
+const path = require('path');
+
+import { regions } from './regions';
+
+let i;
+
+const convertRegion = (input: string) => {
+  input = input.toUpperCase();
+  for (i = 0; i < regions.length; i++) {
+    if (regions[i][1] == input) {
+      return regions[i][0];
+    }
+  }
+};
+
+convertRegion.toAbbr = (input: string) => {
+  input = input.replace(/\w\S*/g, function (txt) {
+    return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+  });
+  for (i = 0; i < regions.length; i++) {
+    if (regions[i][0] == input) {
+      return regions[i][1];
+    }
+  }
+};
+
+module.exports = convertRegion; 
